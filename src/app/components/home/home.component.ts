17b667b407b59a55b1409de75968b5e52b976563
Javier Estevez <javierdaniloestevez@gmail.com>
2021-07-20 04:11:04 -0300

    probando peticiones http.get

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor( private http: HttpClient ) {
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .subscribe( (data: any[]) => {
+        this.paises = data;        
+      })
+  }
 
   ngOnInit(): void {
   }
